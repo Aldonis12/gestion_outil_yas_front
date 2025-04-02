737b9441d173891f64481ff1551a3490068c2710
Aldonis12 <micklewis.aldonis@gmail.com>
2025-04-02 10:45:28 +0300

    Auth Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { TableauEbsComponent } from './pages/tableau-ebs/tableau-ebs.component';
 import { EbsFormComponent } from './pages/ebs-form/ebs-form.component';
 import { SiteCandidatComponent } from './pages/site-candidat/site-candidat.component';
 import { DetailSiteComponent } from './pages/detail-site/detail-site.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
         children: [
           { path: '', component: AuthentificationComponent },
-          { path: 'tableau-ebs', component: TableauEbsComponent, title: 'EBS' },
+          { path: 'tableau-ebs', component: TableauEbsComponent, title: 'EBS', canActivate: [authGuard] },
           { path: 'ajout-ebs', component: EbsFormComponent, title: 'Ajout EBS' },
           { path: 'site-validate', component: SiteCandidatComponent, title: 'Site valide' },
           { path: 'detail_candidat/:code_site', component: DetailSiteComponent, title: 'Detail Site' },
---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const router = inject(Router);
+  if(!auth.isLoggedIn()) {
+    auth.logout();
+    
+    alert("Vous devez être connecté pour accéder à cette page !");
+    router.navigateByUrl('')
+    return false
+  }
+  
+  return true;
+};
